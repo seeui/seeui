c82939cb5fb9f365dbdbc692917d4c66d9424894
cgzero <cgzero@cgzero.com>
2017-08-11 23:32:41 +0800

    编译后的文件不再对 es6 进行转码，由项目根据自己情况配置转码规则

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,27 @@
  * @author cgzero(cgzero@cgzero.com)
  */
 
+// 为了方便各个项目去用，这里不进行 es6 转码，各个项目可以根据自己情况配置转码规则
+// - 对于 js：剔除 js 文件 import css 的部分
+// - 对于 stylus：转义成 css
+
 const gulp = require('gulp');
-const babel = require('gulp-babel');
-const babelHelpers = require('gulp-babel-external-helpers');
+// const babel = require('gulp-babel');
+// const babelHelpers = require('gulp-babel-external-helpers');
 const sourcemaps = require('gulp-sourcemaps');
 const stylus = require('gulp-stylus');
 const removeLines = require('gulp-remove-lines');
 const nib = require('nib');
 
-gulp.task('babel', function () {
+gulp.task('js', function () {
     return gulp.src('src/**/*.js')
         // 去除 js 中嵌入的 css
         .pipe(removeLines({'filters': [
             /import '[\S]+\.styl'/
         ]}))
         .pipe(sourcemaps.init())
-        .pipe(babel())
-        .pipe(babelHelpers('babelHelpers.js', 'umd'))
+        // .pipe(babel())
+        // .pipe(babelHelpers('babelHelpers.js', 'umd'))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('lib'));
 });
@@ -42,4 +46,4 @@ gulp.task('font', function () {
     return gulp.src('src/icon/font/*').pipe(gulp.dest('lib/icon/font'));
 });
 
-gulp.task('build', ['babel', 'stylus', 'font']);
+gulp.task('build', ['js', 'stylus', 'font']);
